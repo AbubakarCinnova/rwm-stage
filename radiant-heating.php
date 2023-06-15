3cd2f912e1af8350b98569b1cae629e27037f739
AbubakarCinnova <amahboob@cinnova.com>
2023-06-15 15:25:10 +0500

    radiat heating updated

--- radiant-heating.php
+++ radiant-heating.php
@@ -1,5 +1,17 @@
 <?php include_once('header.php') ?>
-<title>Eco Friendly | Radiant Heating </title>
+<title>Ecofriendly Mechanical Heating and Cooling| Radiant Floor Heating </title>
+<meta name="description"
+    content=" Radiant heat is without a doubt the most comfortable heat you will ever experience in your Bloomington area home!" />
+
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="classification"
+    content=" Radiant heating, Cooling, Heating &amp; Air conditioning, Heating &amp; Cooling, Air conditioning, Bloomington, Indiana" />
+<meta name="resource-type" content="document" />
+<meta name="distribution" content="Global" />
+<meta name="location" content="USA, IN, Bloomington" />
+<meta name="author" content="RW" />
 
 <?php include_once('menu.php') ?>
 </head>
@@ -13,80 +25,64 @@
                     style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 20px;">
                     <div class="  content-area">
                         <div class="row">
-                            <p><img src="<?php echo $base_url; ?>/img/Radiant Floor Heating-min.JPG"
-                                    class="img-responsive">
+                            <p><img src="<?php echo $base_url; ?>/img/eco freindlyv4_1000X350-1.jpg"
+                                    style="width:100%;">
                             </p>
                             <div class="col-md-12">
 
-                                <h1>Radiant Heating</h1>
+                                <h2>Experience Unmatched Comfort and Efficiency with Radiant Floor Heating</h2>
 
-                                <p>Radiant heat is without a doubt the most comfortable heat you will ever experience in
-                                    your home! Radiant heating keeps your home warm, doesn’t make any noise and it’s
-                                    highly
-                                    energy efficient. You can heat all of your house or just a room or two.
-                                </p>
-                                <p>With radiant heat, the heat rises evenly from the floor to the ceiling, so the
-                                    temperature of the room is uniform from top to bottom, so no cold spots. Also,
-                                    there’s
-                                    no blowing air like with typical forced air systems. That means less stirred-up
-                                    dust,
-                                    allergens, or any blowing air noise.</p>
-                                <p> Radiant heating is initially more expensive than conventional forced air systems,
-                                    but
-                                    over time saves money on your utility bills due to much higher efficiency. Since
-                                    there
-                                    are no ducts to leak air and waste energy, radiant floor heating saves you on
-                                    heating
-                                    costs from the first day it's installed.</p>
-                                <p>Unlike traditional forced air heating systems or radiators which can make a room feel
-                                    cold at times and too hot at other times, underfloor heating does not overheat a
-                                    room,
-                                    instead, it achieves the perfect temperature set by you using the thermostat.</p>
-                                <p> Radiant heating is best for new construction or remodels as the floor must be
-                                    partially
-                                    removed to install the system.</p>
+                               <p> Experience the utmost comfort with radiant heat in your home! Radiant floor heating
+                                offers a silent and highly energy-efficient solution to keep your entire house or
+                                specific rooms warm.</p>
 
-                                <h2>How does hydronic radiant floor heating work?</h2>
-                                <p>Hot water is circulated through a very durable flexible polymer (PEX) tubing, which
-                                    when
-                                    installed is located under the floor of rooms in your home. A high-efficiency boiler
-                                    is
-                                    the heart of the system, which heats the water that is circulated throughout your
-                                    home.
-                                    Your home's temperature is controlled by a smart thermostat or programmable
-                                    thermostat.</p>
 
-                                <h2>Hydronic Snow Melt Systems</h2>
-                                <p> Snow melt systems utilize the same concept as the radiant heat installed inside your
-                                    home. Hydronic snow melt systems work by circulating warm glycol through (PEX)
-                                    tubing
-                                    installed under your driveway, walkway, patio, or parking areas. This warms the
-                                    medium
-                                    and melts the snow.</p>
 
-                                <h2>Residential and Commercial Applications of Hydronic Snow Melt Systems:</h2>
-                                <ul>
-                                    <li>Driveways</li>
-                                    <li>Sidewalks</li>
-                                    <li>Patios</li>
-                                    <li>Steps</li>
-                                    <li> Wheel Chair Ramps</li>
-                                    <li>Parking Areas</li>
-                                    <li> Courtyards</li>
-                                    <li> Decks</li>
-                                    <li> Porches</li>
-                                </ul>
+                                <p>With radiant floor heating, the warmth evenly rises from the floor to the ceiling,
+                                creating a uniform temperature throughout the room. Say goodbye to cold spots! Unlike
+                                typical forced air systems, there is no blowing air involved, resulting in reduced dust,
+                                allergens, and noise associated with air movement.</p>
+
+
+
+                               <p> While the initial cost of installing radiant heating may be higher than conventional
+                                forced air systems, it proves to be a cost-effective choice in the long run. Its
+                                superior efficiency helps save on utility bills from day one.</p>
+
+
+
+                                <p>Radiant heating is an ideal choice for new construction or remodeling projects, as it
+                                involves partially removing the floor, walls, or ceiling for installation. This allows
+                                for seamless integration and optimal performance of the system. Moreover, by combining
+                                radiant heating with geothermal energy, you can enjoy the benefits of both heating and
+                                cooling capabilities, along with exceptional energy efficiency.</p>
+
+
+
+                               <p> Unlike traditional forced air heating systems or radiators that can result in
+                                temperature fluctuations and discomfort, radiant heating maintains the perfect
+                                temperature as set by you using the smart thermostat. Bid farewell to rooms feeling too
+                                hot or too cold—radiant floor heating provides consistent and customizable warmth
+                                throughout your home.</p>
+
+
+
+                               <p> Hydronic radiant heating operates by circulating hot water through a durable plastic
+                                tubing system installed beneath the floors of your home's rooms. The heart of the system
+                                is a high-efficiency boiler, geothermal system, or solar water heater, responsible for
+                                heating the water that flows throughout your home. The temperature control is managed by
+                                a smart thermostat, allowing you to regulate the desired level of warmth in your living
+                                spaces.</p>
+
+
+                                <br>
                                 <br>
 
-
-                                <p>For more information on Radiant Heating or Snow Melt Systems please contact us.</p>
-                                <br>
-
-                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us </a>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request Quote </a>
 
                                 <br> <br>
 
-                                <p><strong>When it comes to getting the best heating and cooling equipment or service, the only name you need to know is Ecofriendly.</strong></p>
+                                <p><strong>When it comes to getting the best heating and cooling equipment or service, the only name you need to know in the Bloomington, IN area is Ecofriendly Mechanical.</strong></p>
 
 
                             </div>
