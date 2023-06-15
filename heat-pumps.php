58ed6122f42acb7899470938f1256d8e21fecd00
AbubakarCinnova <amahboob@cinnova.com>
2023-06-15 17:02:31 +0500

    Update heat-pumps.php

--- heat-pumps.php
+++ heat-pumps.php
@@ -1,5 +1,14 @@
 <?php include_once('header.php') ?>
-<title>Ecofriendly Heating and Cooling | Heat Pumps </title>
+<title>Ecofriendly Mechanical Heating and Cooling| Heat Pumps</title>
+<meta name="rating" content="general" />
+<meta name="revisit-after" content="3 days" />
+<meta name="robots" content="index, follow" />
+<meta name="classification"
+    content=" Furnace, Heat Pumps, Cooling, Heating &amp; Air conditioning, dual fuel, Furnace, HVAC, Bloomington, Indiana" />
+<meta name="resource-type" content="document" />
+<meta name="distribution" content="Global" />
+<meta name="location" content="USA, IN, Bloomington" />
+<meta name="author" content="RW" />
 
 <?php include_once('menu.php') ?>
 </head>
@@ -15,28 +24,49 @@
                         <div class="row">
                             <div class="col-md-12">
                                 <h2>Understanding Heat Pumps:</h2>
-                                <p><strong>
-                                        A Dual-Function Heating and Cooling Solution for Michigan Homes
-                                    </strong></p>
+                                <p><strong> A Dual-Function Heating and Cooling Solution for Indiana Homes </strong></p>
 
 
-                                <p>A heat pump is a dual-function system that can both heat and cool a home, making it a versatile option for homeowners in Indiana. Combining a heat pump with a furnace creates a dual fuel system that ensures year-round comfort, with the heat pump operating as an air conditioner in summer and extracting heat from the outdoor air and transferring it inside in winter. </p>
-
-
-
-                                <p>For optimal performance, a heat pump is most effective when temperatures are above 30 degrees Fahrenheit, and a furnace can take over in colder weather. Homeowners in Indiana may need a supplemental heating source when temperatures drop significantly. However, combining a Lennox heat pump with an efficient furnace can create an energy-saving dual-fuel system that reduces gas fuel consumption and ensures maximum comfort on extra-cold days. </p>
-
-
-
-                                <p>Energy efficiency is affected by a variety of factors, including the design, size, and maintenance of the HVAC system, climate, building insulation, and occupants' behavior. Our comfort designers can conduct proper analysis and load calculations to help in selecting the most suitable HVAC system for a home or business.</p>
+                                <p>A heat pump is a dual-function system that can both heat and cool a home, making it a
+                                    versatile option for homeowners in Indiana. Combining a heat pump with a furnace
+                                    creates a dual fuel system that ensures year-round comfort, with the heat pump
+                                    operating as an air conditioner in summer and extracting heat from the outdoor air
+                                    and transferring it inside in winter. </p>
 
 
 
-                                <p>Ecofriendly Heating and Cooling is actively working with customers to get the maximum credit on their new heat pump installations by utilizing the Federal Inflation Reduction Act as well as local Utility Rebates and manufacturer rebates. </p>
+                                <p>To achieve peak performance, a heat pump operates most efficiently when temperatures
+                                    are above 35 degrees Fahrenheit. In colder weather, a furnace becomes the primary
+                                    heating source. In regions like Indiana, where temperatures can significantly drop,
+                                    homeowners may require an additional heating solution. However, by combining a
+                                    Lennox heat pump with a highly efficient furnace, it is possible to establish an
+                                    energy-saving dual-fuel system. This system reduces the consumption of natural gas
+                                    and guarantees optimal comfort even during extremely cold days. </p>
+
+
+
+                                <p>There are several factors that influence energy efficiency, including the design,
+                                    size, and maintenance of the HVAC system, the climate in which the building is
+                                    located, the level of insulation in the building, and the behavior of the occupants.
+                                    At Ecofriendly, our team of comfort designers specializes in conducting thorough
+                                    analysis and load calculations to assist in choosing the most appropriate HVAC
+                                    system for both residential and commercial properties. By considering these
+                                    important factors, we can ensure optimal energy efficiency and comfort for your home
+                                    or business.</p>
+
+                                <p>The amended Energy Efficient Home Improvement Credit, which began in 2023 and extends
+                                    through 2032, increases the tax credits as high as $600 for qualified air
+                                    conditioners or gas furnaces, and up to $2000 for qualified heat pumps, heat pump
+                                    water heaters, or boiler. </p>
+
+                                <p>Ecofriendly Mechanical is actively working with customers to get the maximum credit
+                                    on their new heat pump installations by utilizing the Federal Inflation Reduction
+                                    Act as well as local Utility Rebates and manufacturer rebates. </p>
 
 
                                 <h2>Lennox Model SL25XPV HEAT PUMP</h2>
-                                <p> The most precise and efficient heat pump you can buy. Now featuring Lennox TruHeat Performance.</p>
+                                <p> The most precise and efficient heat pump you can buy. Now featuring Lennox TruHeat
+                                    Performance.</p>
                                 <ul>
                                     <li>Stages of Cooling: Variable-Capacity Operation</li>
                                     <li>Energy Efficiency 24</li>
@@ -67,10 +97,8 @@
                                     <li>Tax Credit Eligible: Yes</li>
                                     <li> Digitally-enabled technology</li>
                                     <li>10-Year Limited Warranty on compressor, 5-Year Limited Warranty on covered
-                                        components.
-                                        This product may be eligible for valuable 10-Year Limited Extended Warranty
-                                        coverage
-                                        with product registration.</li>
+                                        components. This product may be eligible for valuable 10-Year Limited Extended
+                                        Warranty coverage with product registration.</li>
 
                                 </ul>
 
@@ -80,23 +108,27 @@
                                 <h2>Lennox Model EL16XP1 HEAT PUMP</h2>
                                 <p>A highly efficient, single-stage heat pump that delivers exceptional comfort</p>
                                 <ul>
-                                <li>Stages of Cooling: Single Stage</li>
-                        <li>Up to 17 (SEER), Up to 9.8 (HSPF)</li>
-                        <li>ENERGY STAR®</li>
-                        <li>Sound Level As low as 71 dBA</li>
-                        <li>Cabinet PermaGuard Cabinet </li>
-                        <li>R-410A refrigerant</li>
-                        <li>Tax Credit Eligible: Yes</li>
-                        <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year Limited Warranty on covered components. A 10-Year Extended Limited Warranty is available with product registration.</li>
+                                    <li>Stages of Cooling: Single Stage</li>
+                                    <li>Up to 17 (SEER), Up to 9.8 (HSPF)</li>
+                                    <li>ENERGY STAR®</li>
+                                    <li>Sound Level As low as 71 dBA</li>
+                                    <li>Cabinet PermaGuard Cabinet </li>
+                                    <li>R-410A refrigerant</li>
+                                    <li>Tax Credit Eligible: Yes</li>
+                                    <li>Elite models come with a 10-Year Limited Warranty on compressors and a 5-Year
+                                        Limited Warranty on covered components. A 10-Year Extended Limited Warranty is
+                                        available with product registration.</li>
                                 </ul>
                                 <br>
 
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request
+                                    Quote</a>
+                                <br><br>
+                                <p><strong> When it comes to getting the best heating and cooling equipment or service,
+                                        the only name you need to know in the Bloomington, IN area is Ecofriendly
+                                        Mechanical.</strong></p>
 
-                                <p><strong> When it comes to getting the best heating and cooling equipment or service, the only name you need to know is Ecofriendly Heating and Cooling.</strong></p>
 
-                            
-                                <br>
-                               <!--  <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Request Quote</a> -->
                             </div>
                         </div>
                     </div>
