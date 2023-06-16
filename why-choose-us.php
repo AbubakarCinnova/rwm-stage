a91162805149db21bd7e1957942fcb3abfb262fb
AbubakarCinnova <amahboob@cinnova.com>
2023-06-16 12:24:44 +0500

    Update why-choose-us.php

--- why-choose-us.php
+++ why-choose-us.php
@@ -16,61 +16,30 @@
                             <div class="col-lg-12">
 
                                 <h1>Why Choose Us</h1>
-                                <p>At 911 Mechanical Heating and Cooling, we are committed to providing our customers
-                                    with the best possible service experience.</p>
-                                <p><strong>Here's what you'll get with a 911 Mechanical technician that you can't find
-                                        anywhere else:</strong></p>
+                                <p>At Ecofriendly Mechanical Heating and Cooling, we are committed to providing our customers with the best possible service experience.</p>
+                                <p><strong>Here's what you'll get with an Ecofriendly Mechanical technician that you can't find anywhere else:</strong></p>
 
                                 <h2>Professional
                                 </h2>
-                                <p>911 Mechanical demands the highest level of professionalism in each facet of our
-                                    company. From the back-office service staff that arranges your service appointment,
-                                    to the clean, well-stocked truck that arrives in your driveway, to the uniformed,
-                                    clean-cut, polished technician that rings your doorbell -- you'll KNOW you've made
-                                    the right choice to deal with a company that prioritizes service and professionalism
-                                    above all else.</p>
+                                <p>At Ecofriendly Mechanical, we pride ourselves on maintaining the utmost level of professionalism in every aspect of our company. From our dedicated back-office service staff who handle your appointments to our well-equipped trucks that arrive at your doorstep, we ensure that you have a seamless experience.  When you choose us, you can be confident that you have made the right decision in partnering with a company that prioritizes exceptional service and professionalism above all else.</p>
 
                                 <h2>Fair and Honest</h2>
-                                <p>When you work with 911 Mechanical, you'll know you are getting exactly what you've
-                                    paid for. You can rest easy knowing our technicians will never take advantage of you
-                                    and your patronage of our company by overcharging you for a repair. What's more,
-                                    after the initial diagnostic of your problem, our technicians will stop and educate
-                                    you on the required repair and secure your authorization before moving forward.</p>
+                                <p>When you work with Ecofriendly Mechanical, you'll know you are getting exactly what you've paid for. You can rest easy knowing our technicians will never take advantage of you and your patronage of our company by overcharging you for a repair. What's more, after the initial diagnostic of your problem, our technicians will stop and educate you on the required repair and secure your authorization before moving forward.</p>
 
                                 <h2>24/7/365 Availability</h2>
-                                <p>Heating and cooling emergencies can happen any time of the day (or night), so we make
-                                    ourselves available 24/7 for your convenience. We have an answering service at night
-                                    and on weekends to page our on-call technicians. During normal business hours, we
-                                    have a friendly customer service team to make scheduling with us very easy. We have
-                                    a team of residential and commercial technicians - so you can count on us 24/7/365
-                                    for heating and cooling emergencies.</p>
+                                <p>Heating and cooling emergencies can happen any time of the day (or night), so we make ourselves available 24/7 for your convenience. We have an answering service at night and on weekends to page our on-call technicians. During normal business hours, we have a friendly customer service team to make scheduling with us very easy. We have a team of residential and commercial technicians - so you can count on us 24/7/365 for heating and cooling emergencies.</p>
 
                                 <h2>We Stand Behind Our Work</h2>
-                                <p>Every repair that 911 Mechanical performs is handled with attentiveness and care. We
-                                    believe in doing it right the first time and making sure that our customers get the
-                                    most value for their money.</p>
+                                <p>Every repair that Ecofriendly Mechanical performs is handled with attentiveness and care. We believe in doing it right the first time and making sure that our customers get the most value for their money.</p>
 
                                 <h2>We Are A Locally Owned And Operated Company</h2>
-                                <p>911 Mechanical has been around for more than 23 years and will be around for many
-                                    more. We live, work, and play where you do, and we're committed to keeping homes and
-                                    businesses in our community a great place to live and work. We're always close by
-                                    when you need us.</p>
+                                <p>Ecofriendly Mechanical has been around for more than four decades and will be around for many more. We live, work, and play in Bloomington where you do, and we're committed to keeping homes and businesses in our community a great place to live and work. We're always close by when you need us.</p>
 
-                                <h2>Trusted Technicians</h2>
-                                <p>All of our staff will arrive in a marked service truck and wear 911 Mechanical
-                                    uniforms. They have been background checked and drug screened and receive continued
-                                    random drug tests to give you peace of mind when you allow a 911 Mechanical Pro in
-                                    your home.</p>
-
+                        
                                 <h2>Certified Technicians</h2>
-                                <p>Our HVAC technicians are North American Technician Excellence (NATE) certified.
-                                    NATE-certified technicians have demonstrated their knowledge of today's increasingly
-                                    sophisticated heating and cooling systems by passing a nationally recognized test
-                                    developed and supported by all segments of the HVAC industry. Nationally, only about
-                                    1 in 5 technicians are NATE certified.</p>
+                                <p>Our HVAC technicians are North American Technician Excellence (NATE) certified. NATE-certified technicians have demonstrated their knowledge of today's increasingly sophisticated heating and cooling systems by passing a nationally recognized test developed and supported by all segments of the HVAC industry. Nationally, only about 1 in 5 technicians are NATE certified.</p>
 
-                                <p><strong>When it comes to getting the best heating and cooling equipment or service,
-                                        the only name you need to know is 911 Mechanical.</strong></p>
+                                <p><strong>When it comes to getting the best heating and cooling equipment or service, the only name you need to know in the Bloomington, IN area is Ecofriendly Mechanical.</strong></p>
 
 
 
