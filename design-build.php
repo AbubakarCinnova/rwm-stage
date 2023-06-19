231b62552c0e1453f82bd93158c6dfd898837b3a
AbubakarChaudhary <mahboobabubakar786@gmail.com>
2023-06-19 16:15:07 +0500

    updated design build

---
+++ design-build.php
@@ -0,0 +1,95 @@
+<?php include_once('header.php') ?>
+<title>Ecofriendly Mechanical | Design Build</title>
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page page-id-9 page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px;">
+                    <div class="  content-area">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <img src="<?php echo $base_url ?>/img/design-build.php" alt="design-build-baner"
+                                    style="width:100%;">
+                                <br>
+
+                                <h2>Cutting-Edge Renewable Energy Solutions Integrated with Comprehensive Mechanical
+                                    Services in Bloomington, Indiana</h2>
+
+                                <p>Ecofriendly Mechanical is a leading full-service company that specializes in
+                                    providing
+                                    comprehensive mechanical solutions for the commercial, industrial, and institutional
+                                    markets. Our expertise extends to design/build projects, HVAC systems, refrigeration
+                                    solutions, and cutting-edge renewable energy technologies. Located in Bloomington,
+                                    Indiana, we proudly serve the local area and surrounding regions.</p>
+
+
+
+                                <p>At Ecofriendly Mechanical, sustainability is at the core of our approach. Our
+                                    multidisciplinary team is highly skilled in all aspects of mechanical systems,
+                                    including
+                                    the integration of renewable energy technologies. We excel in delivering plan and
+                                    spec
+                                    projects, design-build projects, as well as service and preventive maintenance for
+                                    HVAC
+                                    systems. By harnessing the power of renewable resources, we design, build, and
+                                    retrofit
+                                    highly energy-efficient building systems that align with your sustainability goals.
+                                </p>
+
+
+
+                                <p>With our commitment to excellence and customer satisfaction, we strive to provide
+                                    top-quality service and innovative solutions to our environmentally-conscious
+                                    customer
+                                    base. Our expert technicians and effective project management ensure seamless
+                                    execution,
+                                    while our competitive prices make sustainable practices accessible to all. Since
+                                    1982,
+                                    we have been dedicated to delivering quality Design Build, HVAC, and Refrigeration
+                                    services, empowering our customers to lower operating costs, increase energy
+                                    efficiency,
+                                    and embrace sustainable practices in both new and existing buildings.</p>
+
+
+
+                                <p><strong>Choose Ecofriendly Mechanical for cutting-edge renewable energy solutions
+                                        integrated
+                                        with comprehensive mechanical services. Experience the benefits of sustainable
+                                        technologies while receiving exceptional service tailored to your unique
+                                        requirements.</strong></p>
+
+                                <br>
+
+                                <ul>
+                                    <li>Renewable Energy Technologies</li>
+                                    <li>Job Costing</li>
+                                    <li>Service and Maintenance</li>
+                                    <li>Value Engineering</li>
+                                    <li>Design/Build Projects</li>
+                                    <li>Complete Installation of HVAC systems</li>
+                                    <li>Duct Systems and Modifications</li>
+                                    <li>Energy Analysis</li>
+                                    <li>Heating Loss/Gain Load Calculations</li>
+                                </ul>
+
+                                <br><br>
+
+
+
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning">Contact Us</a>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
