bce521ed30aaaf4091afd6ce27b1aeaaee0bc0cc
AbubakarChaudhary <mahboobabubakar786@gmail.com>
2023-06-20 12:20:04 +0500

    standby updated

---
+++ standby-generators.php
@@ -0,0 +1,80 @@
+<?php include_once('header.php') ?>
+<title>Ecofriendly Mechanical | Standby Generators | Bloomington, IN</title>
+<meta name="description"
+    content=" We proudly serve residents in the Bloomington, Indiana area, providing exceptional solutions for all your home or commercial standby generator needs." />
+<meta name="keywords"
+    content="standby generator, home generator, generator repair, generator installation, generator sales, Generac, power outage home, home standby generator" />
+<?php include_once('menu.php') ?>
+
+</head>
+
+<body class="internal page  page-template page-template-home-php">
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 30px;">
+                    <div class=" content-area specials-page">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <img src="<?php echo $base_url ?>/img/Generac-Banner.jpg" alt="Generac"
+                                    class="img-responsive">
+                                <br>
+                                <h2> Reliable Automatic Standby Generators for Homes and Businesses</h2>
+
+                                <p>Ensuring an uninterrupted power supply is crucial for the smooth functioning of homes
+                                    and businesses. That's why having a dependable automatic standby generator is
+                                    essential.
+                                    No one wants to face the inconvenience of being without lights, air conditioning,
+                                    heating, and other essential appliances during a power outage.</p>
+
+
+                                <h2>Experience Peace of Mind During Power Outages</h2>
+
+                                <p>Modern generators are designed to operate quietly while providing power to your
+                                    entire
+                                    home or business. Whole-house generators can keep your lights, security system, sump
+                                    pump, television, healthcare equipment, heating and cooling systems, and even the
+                                    refrigerator running smoothly during electrical disruptions. An automatic standby
+                                    generator is equipped to detect power cuts and seamlessly starts up within seconds
+                                    to
+                                    restore full electrical power to your premises. This level of reliability offers you
+                                    true peace of mind.</p>
+
+                                <img src="<?php echo $base_url ?>/img/gen-1.jpg" alt="Generac-2"
+                                    class="service-area-pages-banner">
+
+                                <p> As an authorized dealer of Generac standby generators, Ecofriendly Mechanical
+                                    specializes in evaluating your home's wiring and power requirements. Our team will
+                                    present you with a range of high-quality and recommended home power generators that
+                                    align with your specific needs and budget.
+                                </p>
+
+                                <p>Ecofriendly Mechanical's expert technicians are dedicated to delivering the finest
+                                    sales, repair, and installation services for both commercial and residential
+                                    generators. We proudly serve residents in the Bloomington, Indiana area, providing
+                                    exceptional solutions for all your home or commercial standby generator needs.
+                                </p>
+
+                                <br>
+                                <a href="<?php echo $base_url ?>/contact-us.php" class="btn btn-warning"> Contact Us
+                                </a>
+                                <br><br>
+
+                            </div>
+
+                            <div class="col-md-6 iframe-wrap">
+                                <iframe width="400" height="300" src="https://www.youtube.com/watch?v=ASvJtN-4Z9s"
+                                  title="Ecofriendly Bloomington" frameborder="0"
+                                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                  allowfullscreen></iframe>
+                    
+                              </div>
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
